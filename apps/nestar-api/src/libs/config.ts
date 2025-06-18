4be33c7610b9b3dbc9a443d260e034f0f9a46514
Odilov Sarvarbek <adiof.sarvar@gmail.com>
2025-06-18 17:21:43 +0900

    feat: develop getAgents GraphQL api

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -1,5 +1,7 @@
 import { ObjectId } from 'bson';
 
+export const availabeAgentSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberView', 'memberRank'];
+
 export const shapeIntoMongoObjectId = (target: any) => {
 	return typeof target === 'string' ? new ObjectId(target) : target;
 };
