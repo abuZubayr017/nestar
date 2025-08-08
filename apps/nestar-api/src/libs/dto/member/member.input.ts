4b2ca7c9110e316fe34ffbe61a185d3a3ab55cf2
Odilov Sarvarbek <adiof.sarvar@gmail.com>
2025-08-08 20:47:20 +0900

    fix: modify getAllMembersByAdmin

--- apps/nestar-api/src/libs/dto/member/member.input.ts
+++ apps/nestar-api/src/libs/dto/member/member.input.ts
@@ -103,7 +103,7 @@ export class MembersInquiry {
 	limit: number;
 
 	@IsOptional()
-	@IsIn([availabeMemberSorts])
+	@IsIn(availabeMemberSorts)
 	@Field(() => String, { nullable: true })
 	sort?: string;
 
